0f6e20ea1c4de42b0b476e939b81322cd0235c60
Trung <profile.nvt@gmail.com>
2020-08-05 23:05:49 +0700

    Sửa lại giao diện add bot

--- app/Jobs/BotFacebook.php
+++ app/Jobs/BotFacebook.php
@@ -38,8 +38,8 @@ class BotFacebook implements ShouldQueue
             return;
         }
         /*
-        if (empty($bot->proxy)) {
-            Log::error("Bot ID ".$bot->id." không có Proxy nên ko dám chạy kẻo bay Acc :D");
+        if (empty($bots->proxy)) {
+            Log::error("Bot ID ".$bots->id." không có Proxy nên ko dám chạy kẻo bay Acc :D");
             return;
         }
         */
@@ -58,17 +58,9 @@ class BotFacebook implements ShouldQueue
             return;
         }
 
-        // Chọn theo cảm xúc khách setup, nếu khách setup số linh tinh thì chọn random 1 trong các cảm xúc
-        $reactions = array(1, 2, 3, 4, 6, 8, 16);
-        if (in_array($bot->reaction_type, $reactions)) {
-            $reactionType = $bot->reaction_type;
-        } else {
-            $reactionType = $reactions[rand(0, count($reactions) - 1)];
-        }
 
-        // Chọn $postId theo quy tắc đã setup
+        $reactionType = rand(1, 4);
         $postId = '2613233488893674';
-
         $reaction = reactionPostByCookie($bot->cookie, $fbDtg, $postId, $reactionType, $bot->proxy);
         if ($reaction) {
             $botLog = new BotLog();
@@ -77,48 +69,25 @@ class BotFacebook implements ShouldQueue
             $botLog->post_id = $postId;
             $botLog->save();
 
-            // Update next run time of bot
+            // Update next run time of bots
             $bot->next_run_time = time() + $bot->frequency * 60;
             $bot->save();
         }
 
-        // Nếu bật Auto comment
-        if ($bot->comment_on) {
-            // Build nội dung comment
-            $commentContent = RandomComment();
+        $commentContent = RandomComment();
+        $postId = '2613233488893674';
+        $comment = commentPostByCookie($bot->cookie, $fbDtg, $postId, $commentContent, null, $bot->proxy);
+        if ($comment) {
+            $botLog = new BotLog();
+            $botLog->bot_id = $bot->id;
+            $botLog->action = 'COMMENT';
+            $botLog->comment_id = $comment;
+            $botLog->post_id = $postId;
+            $botLog->save();
 
-            // Random Sticker ID nếu người dùng có chọn collection
-            $stickerId = null;
-            if (!empty($bot->sticker_collection_id)) {
-                $tmpStickerId = randomStickerOfCollection($bot->cookie, $fbDtg, $bot->sticker_collection_id);
-                if ($tmpStickerId !== false) {
-                    $stickerId = $tmpStickerId;
-                }
-            }
-
-            // Gửi comment
-            $comment = commentPostByCookie($bot->cookie, $fbDtg, $postId, $commentContent, $stickerId, $bot->proxy);
-            if ($comment) {
-                $botLog = new BotLog();
-                $botLog->bot_id = $bot->id;
-                $botLog->action = 'COMMENT';
-                $botLog->comment_id = $comment;
-                $botLog->comment_content = $commentContent;
-                $botLog->post_id = $postId;
-                $botLog->save();
-
-                // Update thời gian chạy lần tiếp theo. Có thể sai lệch 25% so với thời gian setup để trông tự nhiên hơn
-                if ($bot->start_time > $bot->end_time) {
-                    // Trường hợp chạy đêm, ví dụ 23h hôm trc tới 8h sáng hôm sau
-                    $start_time = strtotime('yesterday +'.$bot->start_time.'hours');
-                    $end_time = strtotime('today +'.$bot->end_time.'hours');
-                } else {
-                    $start_time = strtotime('today +'.$bot->start_time.'hours');
-                    $end_time = strtotime('today +'.$bot->end_time.'hours');
-                }
-                $bot->next_run_time = min(max($start_time, time() + $bot->frequency * rand(75, 125) / 100 * 60), $end_time);
-                $bot->save();
-            }
+            // Update next run time of bots
+            $bot->next_run_time = time() + $bot->frequency * 60;
+            $bot->save();
         }
     }
 }
